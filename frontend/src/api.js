0ef64d5579a90d81f6c94e6d26b3768863cbcef6
Monisha J <monisha.j.204@gmail.com>
2026-06-03 03:36:52 +0530

    Update frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api/chat', 
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/chat`,
 });
 
 export const createOrGetChat = (userId1, userId2) => {
