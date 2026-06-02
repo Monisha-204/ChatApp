935a2cf45511e6a6facbab659678d9ae8a775f62
Monisha J <monisha.j.204@gmail.com>
2026-06-03 03:10:52 +0530

    Update backend

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,10 @@ server.listen(PORT, () => {
 // ────────────────────────────── Socket.IO Setup ──────────────────────────────
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "https://chatapp-lk00.onrender.com/"
+    ],
     methods: ["GET", "POST"]
   }
 });
