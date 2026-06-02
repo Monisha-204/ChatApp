5c1f3626dcb4d4596063eb0f8e7c7bda88b175e9
Monisha J <monisha.j.204@gmail.com>
2026-06-03 04:02:37 +0530

    Minor updates

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,13 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: [
+    "http://localhost:5173",
+    "https://chatapp-lk00.onrender.com"
+  ],
   credentials: true
 }));
+
 app.use(express.json({ limit: '10mb' })); 
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
@@ -41,7 +45,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://chatapp-lk00.onrender.com/"
+      "https://chatapp-lk00.onrender.com"
     ],
     methods: ["GET", "POST"]
   }
